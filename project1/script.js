8d39cf9b4ab260f9d90db8a22e5fe02113ec9b5e
Ravi Vanam <ravivanam9922@gmail.com>
2022-01-04 17:18:35 +0530

    did project 1

--- project1/script.js
+++ project1/script.js
@@ -1,1 +1,28 @@
+let load = 0;
+const loadText = document.querySelector(".loading-text");
+const bg = document.querySelector(".bg");
 
+function mouseOver() {
+    loadText.style.color = "red";
+}
+
+function mouseOut() {
+    loadText.style.color = "white";
+}
+
+function onClick() {
+    let interval = setInterval(() => {
+        load++;
+    
+        loadText.innerHTML = `${load}%`;
+        loadText.style.opacity = (100 - load)/100;
+        bg.style.filter = `blur(${(30 * ((100 - load) / 100))}px)`
+    
+        if (load > 99) {
+            clearInterval(interval);
+            loadText.innerHTML = "TADA!";
+            loadText.style.fontSize = "100px";
+            loadText.style.opacity = 1;
+        }
+    }, 30);
+}
