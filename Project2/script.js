29263d26f14c24f2fa5d14e26ed12fad11f15253
Ravi Vanam <vanamravi9922@gmail.com>
2022-01-06 20:12:15 +0530

    modularized the code a bit

--- Project2/script.js
+++ Project2/script.js
@@ -2,14 +2,34 @@ const sliderContainer = document.querySelector('.slider-container');
 const slideLeft = document.querySelector('.left-slide');
 const slideRight = document.querySelector('.right-slide'); 
 const upButton = document.querySelector('.arrow-up');
-const downButton = document.querySelector('.arrow-down'); 
-const slidesLength = slideRight.querySelectorAll('div').length;
+const downButton = document.querySelector('.arrow-down');
+
+// ---------------testing-------------------
+const createNewSlide = function(slide) {
+    slideLeft.innerHTML = `<div style="background-color: ${slide.slideColor};">\
+                                <h1 style="color: ${slide.nameColor};">${slide.name}</h1>\
+                                    <div class="quote">\
+                                        <p>${slide.quote}</p>\
+                                    </div>\
+                                </div>`
+                            + slideLeft.innerHTML;
+
+    slideRight.innerHTML = slideRight.innerHTML 
+                            + `<div style="background-image: url(${slide.imageURL});"></div>`;
+}
+                        
+const slidesLength = slides.length;
+for (let i = 0; i < slidesLength; i++) {
+    createNewSlide(slides[i]);
+}
+
+// ----------------working -----------------
 
 let activeSlideIndex = 0;
 
 slideLeft.style.top = `-${(slidesLength - 1) * 100}vh`;
 
-upButton.addEventListener('click', () => changeSlide('up'));
+upButton.addEventListener('click', () => changeSlide('up')); 
 downButton.addEventListener('click', () => changeSlide('down'));
 
 const changeSlide = direction => {
---
+++ Project2/slides_config.js
@@ -0,0 +1,30 @@
+const slides = [
+    {
+        "name" : "Armin Arlert",
+        "quote": "Someone who cannot abandon everything cannot achieve anything.",
+        "imageURL": "images/armin.webp",
+        "nameColor": "yellow",
+        "slideColor": "rgb(160, 36, 36)"
+    },
+    {
+        "name" : "Eren Yeager",
+        "quote": "If you win, you live. If you lose, you die. If you don't fight, you can't win.",
+        "imageURL": "images/eren.jpg",
+        "nameColor": "rgb(78, 190, 25)",
+        "slideColor": "rgb(64, 42, 104)"
+    },
+    {
+        "name" : "Mikasa Ackerman",
+        "quote": "This world is cruel and merciless. But it's also very beautiful at the same time.",
+        "imageURL": "images/mikasa.jpg",
+        "nameColor": "rgb(180, 32, 32)",
+        "slideColor": "rgb(78, 63, 34)"
+    },
+    {
+        "name" : "Levi Ackerman",
+        "quote": "Whether I trusted myself or the choices of my dependable comrades, there was no telling how things would turn out.",
+        "imageURL": "images/levi.png",
+        "nameColor": "rgb(68, 176, 209)",
+        "slideColor": "rgb(14, 34, 36)"
+    }
+]
